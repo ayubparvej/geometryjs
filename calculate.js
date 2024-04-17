45a70b5750754692a2fea76b310fc67f78c1d4f3
ayubparvej <ayub.pervej1@gmail.com>
2024-04-17 17:20:30 +0600

    done

--- calculate.js
+++ calculate.js
@@ -1,20 +1,40 @@
 
-function triangleLength() {
+// function triangleLength() {
 
-    const tb = document.getElementById('triangle-b');
+//     const tb = document.getElementById('triangle-b');
+//     const tbValue = tb.value;
+//     const base = parseFloat(tbValue);
+
+
+//     const th = document.getElementById('triangle-h');
+//     const thValue = th.value;
+//     const height = parseFloat(thValue);
+
+//     const area = 0.5 * base * height;
+
+//     const h2 = document.createElement('h2');
+//     h2.innerText = 'The area of the triangle is ' + area;
+
+//     document.getElementById('triangle-area').appendChild(h2);
+
+// }
+
+
+function triangleLength() {
+    const base = getInputValueByID('triangle-b')
+    const height = getInputValueByID('triangle-h')
+
+}
+
+function getInputValueByID(inputValue) {
+
+    const tb = document.getElementById(inputValue);
     const tbValue = tb.value;
     const base = parseFloat(tbValue);
+    return base;
+}
 
-
-    const th = document.getElementById('triangle-h');
-    const thValue = th.value;
-    const height = parseFloat(thValue);
-
-    const area = 0.5 * base * height;
-
-    const h2 = document.createElement('h2');
-    h2.innerText = 'The area of the triangle is ' + area;
-
-    document.getElementById('triangle-area').appendChild(h2);
+function setInnerText(innerTextValue) {
+    const text = document.getElementById(innerTextValue);
 
 }
